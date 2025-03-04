1e5868e6bd4d049c6528a74463ed1c8b2538cfc6
예령 <gimdPfud@gmail.com>
2025-03-04 10:57:52 +0900

    3월 4일 오전 실습. 주석 조금 적음.

--- src/test/java/com/example/shop/ShopApplicationTests.java
+++ src/test/java/com/example/shop/ShopApplicationTests.java
@@ -3,11 +3,35 @@ package com.example.shop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootTest
 class ShopApplicationTests {
 
     @Test
     void contextLoads() {
     }
+
+    @Test
+    void listPractice(){
+        List<String> list = new ArrayList<>(Arrays.asList("A","B","C"));
+        list.add("D");
+
+        for (int i = 0; i < list.toArray().length; i++) {
+            if(list.get(i).equals("B")){
+                System.out.println(list.get(i));
+            }
+        }
+
+        for (String s : list) {
+            if(s.equals("B")){
+                System.out.println(s);
+            }
+        }
+
+    }
+
 
 }
